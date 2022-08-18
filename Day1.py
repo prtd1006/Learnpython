c7731bf84934b2f9a87d2d2b85841bbd0b1d912e
prtd1006 <rtd982@gmail.com>
2022-08-18 18:27:32 +0530

    Day1 Half Done till 8th Videos

--- Day1.py
+++ Day1.py
@@ -2,4 +2,5 @@
 #print("hello world")
 #print('Hello "hey" World')
 #print("hello 'hi' World")
-print("Hello Rohit\nHello Ashish\nHello Raj")
+#print("Hello Rohit\nHello Ashish\nHello Raj")
+print("Hello" + " " + "Rohit")
